554c08ebf603821788345a5bb60f4159741f784c
Breno Toledo <66034497+BrenoToledo@users.noreply.github.com>
2023-03-26 11:18:24 -0300

    Projeto da calculadora de volume
    Projeto em C# para calcular e manipular elementos de formulário.

---
+++ Atividade 1/Pvolume/Pvolume/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace Pvolume
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.btnSair = new System.Windows.Forms.Button();
+            this.lblRaio = new System.Windows.Forms.Label();
+            this.lblAltura = new System.Windows.Forms.Label();
+            this.lblVolume = new System.Windows.Forms.Label();
+            this.txtRaio = new System.Windows.Forms.TextBox();
+            this.txtAltura = new System.Windows.Forms.TextBox();
+            this.txtVolume = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(22, 259);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(113, 92);
+            this.btnCalcular.TabIndex = 0;
+            this.btnCalcular.Text = "CALCULAR";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // btnSair
+            // 
+            this.btnSair.Location = new System.Drawing.Point(164, 259);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(115, 92);
+            this.btnSair.TabIndex = 1;
+            this.btnSair.Text = "FECHAR";
+            this.btnSair.UseVisualStyleBackColor = true;
+            this.btnSair.Click += new System.EventHandler(this.btnSair_Click);
+            // 
+            // lblRaio
+            // 
+            this.lblRaio.AutoSize = true;
+            this.lblRaio.Location = new System.Drawing.Point(41, 39);
+            this.lblRaio.Name = "lblRaio";
+            this.lblRaio.Size = new System.Drawing.Size(29, 13);
+            this.lblRaio.TabIndex = 2;
+            this.lblRaio.Text = "Raio";
+            // 
+            // lblAltura
+            // 
+            this.lblAltura.AutoSize = true;
+            this.lblAltura.Location = new System.Drawing.Point(41, 104);
+            this.lblAltura.Name = "lblAltura";
+            this.lblAltura.Size = new System.Drawing.Size(34, 13);
+            this.lblAltura.TabIndex = 3;
+            this.lblAltura.Text = "Altura";
+            // 
+            // lblVolume
+            // 
+            this.lblVolume.AutoSize = true;
+            this.lblVolume.Location = new System.Drawing.Point(41, 169);
+            this.lblVolume.Name = "lblVolume";
+            this.lblVolume.Size = new System.Drawing.Size(42, 13);
+            this.lblVolume.TabIndex = 4;
+            this.lblVolume.Text = "Volume";
+            // 
+            // txtRaio
+            // 
+            this.txtRaio.Location = new System.Drawing.Point(147, 32);
+            this.txtRaio.Name = "txtRaio";
+            this.txtRaio.Size = new System.Drawing.Size(100, 20);
+            this.txtRaio.TabIndex = 5;
+            this.txtRaio.Validated += new System.EventHandler(this.txtRaio_Validated);
+            // 
+            // txtAltura
+            // 
+            this.txtAltura.Location = new System.Drawing.Point(147, 97);
+            this.txtAltura.Name = "txtAltura";
+            this.txtAltura.Size = new System.Drawing.Size(100, 20);
+            this.txtAltura.TabIndex = 6;
+            this.txtAltura.Validated += new System.EventHandler(this.txtAltura_Validated);
+            // 
+            // txtVolume
+            // 
+            this.txtVolume.Enabled = false;
+            this.txtVolume.Location = new System.Drawing.Point(147, 162);
+            this.txtVolume.Name = "txtVolume";
+            this.txtVolume.Size = new System.Drawing.Size(100, 20);
+            this.txtVolume.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(300, 388);
+            this.Controls.Add(this.txtVolume);
+            this.Controls.Add(this.txtAltura);
+            this.Controls.Add(this.txtRaio);
+            this.Controls.Add(this.lblVolume);
+            this.Controls.Add(this.lblAltura);
+            this.Controls.Add(this.lblRaio);
+            this.Controls.Add(this.btnSair);
+            this.Controls.Add(this.btnCalcular);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Button btnSair;
+        private System.Windows.Forms.Label lblRaio;
+        private System.Windows.Forms.Label lblAltura;
+        private System.Windows.Forms.Label lblVolume;
+        private System.Windows.Forms.TextBox txtRaio;
+        private System.Windows.Forms.TextBox txtAltura;
+        private System.Windows.Forms.TextBox txtVolume;
+    }
+}
+
---
+++ Atividade 1/Pvolume/Pvolume/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Pvolume
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // O método IsValidate não é utilizado, então foi comentado
+        //public bool IsValidate (double txtBoxInput)
+        //{
+        //    double valor;
+        //    if (!double.TryParse(Text, out valor)) {
+        //        MessageBox.Show("Valido ");
+        //    }
+        //    return (true)
+        //    else
+        //    { 
+        //        return false; 
+        //    }   
+        //}
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Não há necessidade de código aqui neste momento
+        }
+
+        // -------------------------------- VALIDAÇÕES ------------------------------
+        private void txtRaio_Validated(object sender, EventArgs e)
+        {
+            double raio;
+
+            if (!double.TryParse(txtRaio.Text, out raio))
+            {
+                MessageBox.Show("O valor informado não é um número válido!", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtRaio.Focus();
+            }
+            else if (raio <= 0)
+            {
+                MessageBox.Show("O raio deve ser um número positivo maior que zero!", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtRaio.Focus();
+            }
+            else
+            {
+                // O valor informado é válido, pode-se prosseguir com a lógica desejada
+            }
+        }
+
+        private void txtAltura_Validated(object sender, EventArgs e)
+        {
+            double altura;
+
+            if (!double.TryParse(txtAltura.Text, out altura))
+            {
+                MessageBox.Show("O valor informado não é um número válido!", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtAltura.Focus();
+            }
+            else if (altura <= 0)
+            {
+                MessageBox.Show("A altura deve ser um número positivo maior que zero!", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtAltura.Focus();
+            }
+            else
+            {
+                // O valor informado é válido, pode-se prosseguir com a lógica desejada
+            }
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            double raio, altura, volume;
+
+            if (!double.TryParse(txtRaio.Text, out raio) || raio <= 0)
+            {
+                MessageBox.Show("O raio deve ser um número positivo maior que zero!", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtRaio.Focus();
+                return;
+            }
+
+            if (!double.TryParse(txtAltura.Text, out altura) || altura <= 0)
+            {
+                MessageBox.Show("A altura deve ser um número positivo maior que zero!", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtAltura.Focus();
+                return;
+            }
+
+            // Cálculo do volume do cilindro
+            volume = Math.PI * Math.Pow(raio, 2) * altura;
+
+            // Exibição do volume na caixa de texto
+            txtVolume.Text = volume.ToString("#,##0.00");
+        }
+
+        private void btnSair_Click(object sender, EventArgs e)
+        {
+            // Encerramento da aplicação
+            Application.Exit();
+        }
+    }
+}
