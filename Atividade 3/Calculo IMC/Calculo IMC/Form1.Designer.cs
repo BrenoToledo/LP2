45122260e011b3462f0582d85384b711e69aacf2
Breno Toledo <66034497+BrenoToledo@users.noreply.github.com>
2023-04-02 10:13:35 -0300

    Calculo de IMC - Atividade 3
    Foi inserido a tratativa de masks, para textbox.

---
+++ Atividade 3/Calculo IMC/Calculo IMC/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace Calculo_IMC
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.lblMenssage = new System.Windows.Forms.Label();
+            this.btnFechar = new System.Windows.Forms.Button();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.mtxtPeso = new System.Windows.Forms.MaskedTextBox();
+            this.mtxtAltura = new System.Windows.Forms.MaskedTextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(52, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "PESO ";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(41, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "ALTURA";
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(44, 74);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(184, 23);
+            this.btnCalcular.TabIndex = 2;
+            this.btnCalcular.Text = "CALCULAR";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // lblMenssage
+            // 
+            this.lblMenssage.AutoSize = true;
+            this.lblMenssage.ForeColor = System.Drawing.Color.Red;
+            this.lblMenssage.Location = new System.Drawing.Point(41, 145);
+            this.lblMenssage.Name = "lblMenssage";
+            this.lblMenssage.Size = new System.Drawing.Size(70, 13);
+            this.lblMenssage.TabIndex = 5;
+            this.lblMenssage.Text = "Bem Vindo(a)";
+            // 
+            // btnFechar
+            // 
+            this.btnFechar.Location = new System.Drawing.Point(140, 103);
+            this.btnFechar.Name = "btnFechar";
+            this.btnFechar.Size = new System.Drawing.Size(88, 23);
+            this.btnFechar.TabIndex = 6;
+            this.btnFechar.Text = "FECHAR";
+            this.btnFechar.UseVisualStyleBackColor = true;
+            this.btnFechar.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Location = new System.Drawing.Point(44, 103);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(88, 23);
+            this.btnLimpar.TabIndex = 7;
+            this.btnLimpar.Text = "LIMPAR ";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // mtxtPeso
+            // 
+            this.mtxtPeso.Location = new System.Drawing.Point(97, 23);
+            this.mtxtPeso.Mask = "990.00";
+            this.mtxtPeso.Name = "mtxtPeso";
+            this.mtxtPeso.Size = new System.Drawing.Size(131, 20);
+            this.mtxtPeso.TabIndex = 8;
+            this.mtxtPeso.MaskInputRejected += new System.Windows.Forms.MaskInputRejectedEventHandler(this.mtxtPeso_MaskInputRejected);
+            this.mtxtPeso.Validated += new System.EventHandler(this.mtxtPeso_Validated);
+            // 
+            // mtxtAltura
+            // 
+            this.mtxtAltura.Location = new System.Drawing.Point(97, 48);
+            this.mtxtAltura.Mask = "0.00";
+            this.mtxtAltura.Name = "mtxtAltura";
+            this.mtxtAltura.Size = new System.Drawing.Size(131, 20);
+            this.mtxtAltura.TabIndex = 9;
+            this.mtxtAltura.Validated += new System.EventHandler(this.mtxtAltura_Validated);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(273, 204);
+            this.Controls.Add(this.mtxtAltura);
+            this.Controls.Add(this.mtxtPeso);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.btnFechar);
+            this.Controls.Add(this.lblMenssage);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Label lblMenssage;
+        private System.Windows.Forms.Button btnFechar;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.MaskedTextBox mtxtPeso;
+        private System.Windows.Forms.MaskedTextBox mtxtAltura;
+    }
+}
+
---
+++ Atividade 3/Calculo IMC/Calculo IMC/Form1.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculo_IMC
+{
+    public partial class Form1 : Form
+    {
+        //⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛VARIÁVEIS GLOBAIS⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛
+        double peso;
+        double altura;
+        double resultado;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        //⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛
+        //⬛                              BOTÕES                                     ⬛
+        //⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            // [X]TODO: Limpar as Entradas e Saida
+
+            mtxtAltura.Clear();
+            mtxtPeso.Clear();
+
+            lblMenssage.Text = "";
+
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            // [X]TODO: Código que fecha a aplicação
+
+            Application.Exit();
+        }
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            lblMenssage.Text = "";
+
+            if ((Double.TryParse(mtxtPeso.Text, out peso)) && (Double.TryParse(mtxtAltura.Text, out altura)))
+            {
+                if (peso <= 0)
+                {
+                    lblMenssage.Text = "Peso inválido!";
+                    lblMenssage.ForeColor = Color.Red;
+                }
+                else if (altura <= 0)
+                {
+                    lblMenssage.Text = "Altura inválida!";
+                    lblMenssage.ForeColor = Color.Red;
+                }
+                else
+                {
+                    resultado = peso / (Math.Pow(altura, 2));
+                    resultado = Math.Round(resultado, 1);
+
+                    if (resultado < 18.5)
+                    {
+                        lblMenssage.Text = "Magreza";
+                        lblMenssage.ForeColor = Color.Red;
+                    }
+                    else if (resultado <= 24.9)
+                    {
+                        lblMenssage.Text = "Normal";
+                        lblMenssage.ForeColor = Color.Green;
+                    }
+                    else if (resultado <= 29.9)
+                    {
+                        lblMenssage.Text = "Sobrepeso";
+                        lblMenssage.ForeColor = Color.Yellow;
+                    }
+                    else if (resultado <= 39.9)
+                    {
+                        lblMenssage.Text = "Obesidade";
+                        lblMenssage.ForeColor = Color.Red;
+                    }
+                    else
+                    {
+                        lblMenssage.Text = "Obesidade grave";
+                        lblMenssage.ForeColor = Color.Red;
+                    }
+                }
+            }
+            else
+            {
+                lblMenssage.Text = "Valores inválidos";
+                lblMenssage.ForeColor = Color.Red;
+            }
+
+        }
+
+        //⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛
+        //⬛                               VALIDAÇÕES                                ⬛
+        //⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛
+        private void mtxtPeso_TextChanged(object sender, EventArgs e)
+        {
+            lblMenssage.Text = "";
+            lblMenssage.ForeColor = Color.Black;
+        }
+
+        private void mtxtAltura_TextChanged(object sender, EventArgs e)
+        {
+            lblMenssage.Text = "";
+            lblMenssage.ForeColor = Color.Black;
+        }
+
+        private void mtxtPeso_MaskInputRejected(object sender, MaskInputRejectedEventArgs e)
+        {
+
+        }
+
+        private void mtxtPeso_Validated(object sender, EventArgs e)
+        {
+            if (!Double.TryParse(mtxtPeso.Text, out peso))
+            {
+                lblMenssage.Text = "";
+                lblMenssage.Text = "Peso inválido!";
+                lblMenssage.ForeColor = Color.Red;
+            }
+        }
+
+        private void mtxtAltura_Validated(object sender, EventArgs e)
+        {
+            if (!Double.TryParse(mtxtAltura.Text, out altura))
+            {
+                lblMenssage.Text = "";
+                lblMenssage.Text = "Altura inválido!";
+                lblMenssage.ForeColor = Color.Red;
+
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lblMenssage.Text = "";
+            lblMenssage.Text = "Bem Vindo(a)";
+            lblMenssage.ForeColor = Color.Black;
+        }
+    }
+}
