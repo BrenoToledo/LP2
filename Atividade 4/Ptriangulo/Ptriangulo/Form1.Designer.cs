e7cdda267a4b276a05ae13f7f2608f00e09b0a81
Breno Toledo <66034497+BrenoToledo@users.noreply.github.com>
2023-04-21 17:59:44 -0300

    Atividade 01 - PTriangulo
    Programa que valida as informações de entrada e caso seja um triângulo ele exibe o tipo de triângulo.

---
+++ Atividade 4/Ptriangulo/Ptriangulo/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace Ptriangulo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblLadoA = new System.Windows.Forms.Label();
+            this.lblLadoB = new System.Windows.Forms.Label();
+            this.lblLadoC = new System.Windows.Forms.Label();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.btnVerificar = new System.Windows.Forms.Button();
+            this.txtLadoA = new System.Windows.Forms.TextBox();
+            this.txtLadoB = new System.Windows.Forms.TextBox();
+            this.txtLadoC = new System.Windows.Forms.TextBox();
+            this.btnSair = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblLadoA
+            // 
+            this.lblLadoA.AutoSize = true;
+            this.lblLadoA.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F);
+            this.lblLadoA.Location = new System.Drawing.Point(273, 180);
+            this.lblLadoA.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.lblLadoA.Name = "lblLadoA";
+            this.lblLadoA.Size = new System.Drawing.Size(70, 24);
+            this.lblLadoA.TabIndex = 0;
+            this.lblLadoA.Text = "Lado A";
+            // 
+            // lblLadoB
+            // 
+            this.lblLadoB.AutoSize = true;
+            this.lblLadoB.Location = new System.Drawing.Point(273, 274);
+            this.lblLadoB.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.lblLadoB.Name = "lblLadoB";
+            this.lblLadoB.Size = new System.Drawing.Size(69, 24);
+            this.lblLadoB.TabIndex = 1;
+            this.lblLadoB.Text = "Lado B";
+            // 
+            // lblLadoC
+            // 
+            this.lblLadoC.AutoSize = true;
+            this.lblLadoC.Location = new System.Drawing.Point(273, 368);
+            this.lblLadoC.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.lblLadoC.Name = "lblLadoC";
+            this.lblLadoC.Size = new System.Drawing.Size(70, 24);
+            this.lblLadoC.TabIndex = 2;
+            this.lblLadoC.Text = "Lado C";
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Location = new System.Drawing.Point(450, 434);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(150, 60);
+            this.btnLimpar.TabIndex = 3;
+            this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // btnVerificar
+            // 
+            this.btnVerificar.Location = new System.Drawing.Point(277, 434);
+            this.btnVerificar.Name = "btnVerificar";
+            this.btnVerificar.Size = new System.Drawing.Size(150, 60);
+            this.btnVerificar.TabIndex = 4;
+            this.btnVerificar.Text = "Verificar";
+            this.btnVerificar.UseVisualStyleBackColor = true;
+            this.btnVerificar.Click += new System.EventHandler(this.btnVerificar_Click);
+            // 
+            // txtLadoA
+            // 
+            this.txtLadoA.Location = new System.Drawing.Point(373, 177);
+            this.txtLadoA.Name = "txtLadoA";
+            this.txtLadoA.Size = new System.Drawing.Size(100, 29);
+            this.txtLadoA.TabIndex = 5;
+            // 
+            // txtLadoB
+            // 
+            this.txtLadoB.Location = new System.Drawing.Point(373, 269);
+            this.txtLadoB.Name = "txtLadoB";
+            this.txtLadoB.Size = new System.Drawing.Size(100, 29);
+            this.txtLadoB.TabIndex = 6;
+            // 
+            // txtLadoC
+            // 
+            this.txtLadoC.Location = new System.Drawing.Point(373, 363);
+            this.txtLadoC.Name = "txtLadoC";
+            this.txtLadoC.Size = new System.Drawing.Size(100, 29);
+            this.txtLadoC.TabIndex = 7;
+            // 
+            // btnSair
+            // 
+            this.btnSair.Location = new System.Drawing.Point(624, 434);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(150, 60);
+            this.btnSair.TabIndex = 8;
+            this.btnSair.Text = "Sair";
+            this.btnSair.UseVisualStyleBackColor = true;
+            this.btnSair.Click += new System.EventHandler(this.btnSair_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1467, 830);
+            this.Controls.Add(this.btnSair);
+            this.Controls.Add(this.txtLadoC);
+            this.Controls.Add(this.txtLadoB);
+            this.Controls.Add(this.txtLadoA);
+            this.Controls.Add(this.btnVerificar);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.lblLadoC);
+            this.Controls.Add(this.lblLadoB);
+            this.Controls.Add(this.lblLadoA);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(5, 6, 5, 6);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblLadoA;
+        private System.Windows.Forms.Label lblLadoB;
+        private System.Windows.Forms.Label lblLadoC;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.Button btnVerificar;
+        private System.Windows.Forms.TextBox txtLadoA;
+        private System.Windows.Forms.TextBox txtLadoB;
+        private System.Windows.Forms.TextBox txtLadoC;
+        private System.Windows.Forms.Button btnSair;
+    }
+}
+
---
+++ Atividade 4/Ptriangulo/Ptriangulo/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ptriangulo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSair_Click(object sender, EventArgs e)
+        {
+            Close(); // Fechar a Aplicação
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            txtLadoA.Clear();  // Limpar os dados do TextBox
+            txtLadoB.Clear();  // Limpar os dados do TextBox
+            txtLadoC.Clear();  // Limpar os dados do TextBox
+        }
+
+        private void btnVerificar_Click(object sender, EventArgs e)
+        {
+            double ladoA;
+            double ladoB;
+            double ladoC;
+
+            if (!double.TryParse(txtLadoA.Text,out ladoA))
+                MessageBox.Show("Lado A inválido");
+            else if (!double.TryParse(txtLadoB.Text, out ladoB))
+                MessageBox.Show("Lado B inválido");
+            else if (!double.TryParse(txtLadoC.Text, out ladoC))
+                MessageBox.Show("Lado C inválido");
+
+            else // Testar se os valores digitados formam um triangulo
+            {
+                if(ladoA < (ladoB + ladoC) && ladoA > Math.Abs (ladoB - ladoC) &&
+                   ladoB < (ladoA + ladoC) && ladoB > Math.Abs (ladoA - ladoC) &&
+                   ladoC < (ladoA + ladoB) && ladoC > Math.Abs (ladoA - ladoB) )
+                
+                { // SE VERDADEIRO
+
+                    // Verificar o Tipo do triângulo
+                    if ((ladoA == ladoB) && (ladoB == ladoC))
+                    {
+                        MessageBox.Show("É um equilátero");
+                    }
+                    else if ((ladoA != ladoB ) && (ladoB != ladoC) && (ladoC != ladoA))
+                    {
+                        MessageBox.Show("É um escaleno");
+                    }
+                    else
+                    {
+                        MessageBox.Show("É um isósceles");
+                    }
+                }
+
+                else // SE FALSO
+                {
+                    MessageBox.Show("Os Lados fornecidos não formam um triângulo");
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
